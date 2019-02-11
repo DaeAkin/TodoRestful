d0452b003726e2e0f20edb50c5e5849faeb7a167
DonghyeonMin <kei890@naver.com>
2019-02-11 18:59:06 +0900

    TodoController CRUD 추가했음.

--- TodoRestful/src/main/java/com/todo/restful/controller/TodoController.java
+++ TodoRestful/src/main/java/com/todo/restful/controller/TodoController.java
@@ -8,8 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.SystemPropertyUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,7 +23,10 @@ import com.todo.restful.service.TodoService;
 @CrossOrigin
 @RestController
 public class TodoController {
-	
+	/* Todo 
+	 * Session 인증 작업은 인터프리터나 프록시를 이용해서 
+	 * Session 인증 할것.
+	 */
 	@Autowired
 	TodoService todoService;
 	
@@ -34,6 +37,7 @@ public class TodoController {
 		session.setAttribute("loginSession", 1234);
 	}
 
+	//해당 아이디에 관련된 todo를 가져오는 메소드
 	@RequestMapping(value="/getAllTodo" , method = RequestMethod.GET)
 	public Map<String, Object> getAllTodo(HttpServletRequest request) throws SessionInvalidException  {
 		
@@ -55,17 +59,49 @@ public class TodoController {
 		resultMap.put("todos", todos);
 		
 		
+		return resultMap;
+	}
+	
+	//Todo 추가
+	@RequestMapping(value="/addTodo" , method = RequestMethod.GET)
+	public Map<String, Object> addTodo(@RequestBody Map<String, Object> paramMap) {
+		System.out.println("---- addTodo ----");
+		// 리턴할 json 
+		Map<String, Object> resultMap = new HashMap<String, Object>();
 		
+		int result = todoService.addTodo(paramMap);
 		
-		
-		
-		
-		
-		
-		
-		
+		resultMap.put("result", result);
 		
 		return resultMap;
 	}
+	
+	//Todo 업데이트 
+	@RequestMapping(value="/updateTodo" , method = RequestMethod.GET)
+	public Map<String, Object> updateTodo(@RequestBody Map<String, Object> paramMap) {
+	
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+
+		int result = todoService.updateTodo(paramMap);
+
+		resultMap.put("result", result);
+
+		return resultMap;
+	}
+	
+	//Todo 삭제
+	@RequestMapping(value="/deleteTodo/{deleted_id}" , method = RequestMethod.GET)
+	public Map<String, Object> deleteTodo(@PathVariable(value="deleted_id") int todo_no) {
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		
+		int result = 
+				todoService.deleteOneTodoWithTodono(todo_no);
+		
+		resultMap.put("result", result);
+		
+		return resultMap;
+	}
+	
+	// 검색은 클라이언트에서 구현
 
 }
