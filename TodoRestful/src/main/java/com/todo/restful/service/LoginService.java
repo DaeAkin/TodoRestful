d87cd32a0b4a7d2bb438e214c9d59deab5bcf003
DonghyeonMin <kei890@naver.com>
2019-03-04 11:17:44 +0900

    add Transactional annotation

--- TodoRestful/src/main/java/com/todo/restful/service/LoginService.java
+++ TodoRestful/src/main/java/com/todo/restful/service/LoginService.java
@@ -2,10 +2,13 @@ package com.todo.restful.service;
 
 import java.util.Map;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import com.todo.restful.dto.MemberDto;
 
+@Transactional
 public interface LoginService {
-	
+	@Transactional(readOnly=true)
 	public MemberDto login(Map<String, Object> paramMap);
 	
 	public int memberAdd(Map<String, Object> paramMap);
